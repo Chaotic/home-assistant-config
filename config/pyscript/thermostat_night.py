0964693365b8cf61855777b11cb55aa3d5dbc908
Chaotic <matt.scoville@gmail.com>
2020-08-05 14:34:35 -0400

    add more comments to thermostat_night add thermostat_day

--- config/pyscript/thermostat_night.py
+++ config/pyscript/thermostat_night.py
@@ -3,14 +3,17 @@ Thermostat night script
 """
 @time_trigger("once(23:00:00)")
 def thermostat_night():
-    """Trigger at 11pm every test example using pyscript."""
-    log.info(f"thermostat Trigger test:")
+    """Trigger at 11pm every night"""
+    log.info(f"Thermostat Night")
+    """Set High Temp"""
     if float(sensor.dark_sky_daytime_high_temperature_0d) > 85.0:
         hightemp = 85
     elif float(sensor.dark_sky_daytime_high_temperature_0d) < 85.0:
         hightemp = 75
+    """Set Low Temp"""
     if float(sensor.dark_sky_overnight_low_temperature_0d) > 50.0:
         lowtemp = 55
     elif float(sensor.dark_sky_overnight_low_temperature_0d) < 50.0:
         lowtemp = 65
+    """Send the actual command to the thermostat"""
     climate.set_temperature(entity_id="climate.radio_thermostat_company_of_america_ct101_thermostat_iris_mode",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
