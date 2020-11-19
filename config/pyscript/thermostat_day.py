f98d906701c3804bb48271c6b14b1bb02b66c83b
Chaotic <matt.scoville@gmail.com>
2020-11-19 09:00:31 -0500

    modify thermostat values add more temp sensors around the house

--- config/pyscript/thermostat_day.py
+++ config/pyscript/thermostat_day.py
@@ -1,7 +1,7 @@
 """
 Thermostat day script
 """
-@time_trigger("once(7:30:00)")
+@time_trigger("once(6:30:00)")
 def thermostat_work_day():
     """Trigger at 7:30am every"""
     """Only trigger on workday"""
@@ -16,11 +16,11 @@ def thermostat_work_day():
             if float(sensor.dark_sky_overnight_low_temperature_0d) > 50.0:
                 lowtemp = 55
             elif float(sensor.dark_sky_overnight_low_temperature_0d) < 50.0:
-                lowtemp = 65
+                lowtemp = 68
             """Send the actual command to the thermostat"""
             climate.set_temperature(entity_id="climate.radio_thermostat_company_of_america_ct101_thermostat_iris_mode",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
 
-@time_trigger("once(8:30:00)")
+@time_trigger("once(7:30:00)")
 def thermostat_nonwork_day():
     """Trigger at 8:30am every"""
     if input_boolean.vacation != 'on':
@@ -34,6 +34,6 @@ def thermostat_nonwork_day():
             if float(sensor.dark_sky_overnight_low_temperature_0d) > 50.0:
                 lowtemp = 55
             elif float(sensor.dark_sky_overnight_low_temperature_0d) < 50.0:
-                lowtemp = 65
+                lowtemp = 68
             """Send the actual command to the thermostat"""
             climate.set_temperature(entity_id="climate.radio_thermostat_company_of_america_ct101_thermostat_iris_mode",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
