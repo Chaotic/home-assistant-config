80ded4f1590a2ef9f5df218641f8200fa8f7e554
Chaotic <matt.scoville@gmail.com>
2020-11-23 14:12:30 -0500

    Update the night script to account for vacation mode.

--- config/pyscript/thermostat_night.py
+++ config/pyscript/thermostat_night.py
@@ -4,15 +4,21 @@ Thermostat night script
 @time_trigger("once(23:00:00)")
 def thermostat_night():
     """Trigger at 11pm every night"""
-    """Set High Temp"""
-    if float(sensor.dark_sky_daytime_high_temperature_0d) > 85.0:
-        hightemp = 75
-    elif float(sensor.dark_sky_daytime_high_temperature_0d) < 85.0:
+    if input_boolean.vacation != 'on':
+        """Set High Temp"""
+        if float(sensor.dark_sky_daytime_high_temperature_0d) > 85.0:
+            hightemp = 75
+        elif float(sensor.dark_sky_daytime_high_temperature_0d) < 85.0:
+            hightemp = 85
+        """Set Low Temp"""
+        if float(sensor.dark_sky_overnight_low_temperature_0d) > 50.0:
+            lowtemp = 55
+        elif float(sensor.dark_sky_overnight_low_temperature_0d) < 50.0:
+            lowtemp = 65
+    elif input_boolean.vacation == 'on':
+        """Set High Temp"""
         hightemp = 85
-    """Set Low Temp"""
-    if float(sensor.dark_sky_overnight_low_temperature_0d) > 50.0:
+        """Set Low Temp"""
         lowtemp = 55
-    elif float(sensor.dark_sky_overnight_low_temperature_0d) < 50.0:
-        lowtemp = 65
-    """Send the actual command to the thermostat"""
+        """Send the actual command to the thermostat"""
     climate.set_temperature(entity_id="climate.radio_thermostat_company_of_america_ct101_thermostat_iris_mode",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
