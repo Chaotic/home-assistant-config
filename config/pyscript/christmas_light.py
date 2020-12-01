dead69ffe5c0fa128ae98db91b01fdf45d5d773a
Chaotic <matt.scoville@gmail.com>
2020-11-30 21:41:04 -0500

    remove custom lovelace I'm no longer using update some automation scripts add a few new custom components Add a new scene for christmas.

--- config/pyscript/christmas_light.py
+++ config/pyscript/christmas_light.py
@@ -5,5 +5,5 @@ tag event trigger test script
 def handle_scanned_tag(tag_id=None, device_id=None, context=None):
     log.info(f"got tag_scanned with tag_id={tag_id}, device_id={device_id}, user_id={context.user_id}")
     if tag_id == "41220ee3-400d-480e-85df-314426688b99": 
-        light.toggle(entity_id="light.christmas_lights", brightness_pct=30, effect="Rainbow")
+        scene.turn_on(entity_id="scene.merry_christmas", transition="0")
         
--- config/pyscript/tag_test.py
+++ config/pyscript/tag_test.py
@@ -8,8 +8,8 @@ def handle_scanned_tag(tag_id=None, device_id=None, context=None):
         log.info(f"got the right tag")
         if context.user_id == "2e85aebd0a9c436c8567c5435f0d0df8":
             log.info(f"Matt Scanned")
-            light.turn_on(entity_id="light.matt_night_light", brightness=255)
+            light.toggle(entity_id="light.matt_night_light", brightness=255)
         elif context.user_id == "2a87a65d647c45bdac0c64045a92fa55":
             log.info(f"Kedar Scanned")
-            light.turn_on(entity_id="light.kedar_night_light", brightness=255)
+            light.toggle(entity_id="light.kedar_night_light", brightness=255)
         
