a8bdd4d9505475849babe54e7b82705707e763ab
Chaotic <matt.scoville@gmail.com>
2021-07-25 20:35:53 -0400

    Make use of guest mode in the thermostat occupancy automation

--- config/pyscript/scripts/thermostat/thermostat_occupancy.py
+++ config/pyscript/scripts/thermostat/thermostat_occupancy.py
@@ -10,19 +10,23 @@ def themostat_left():
     if trig_info["trigger_type"] == "timeout":
         pass
     else:
-        """Set High Temp"""
-        if float(sensor.average_weather_high_temperature) > 85.0:
-            hightemp = 85
-        elif float(sensor.average_weather_high_temperature) < 85.0:
-            hightemp = 85
-        """Set Low Temp"""
-        if float(sensor.average_weather_low_temperature) > 50.0:
-            lowtemp = 55
-        elif float(sensor.average_weather_low_temperature) < 50.0:
-            lowtemp = 55
-        """Send the actual command to the thermostat"""
-        climate.set_temperature(entity_id="climate.thermostat_2",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
+        """ Don't Trigger if guest mode is on """
+        if input_boolean.guest_mode != 'on':
+            """ Set High Temp """
+            if float(sensor.average_weather_high_temperature) > 85.0:
+                hightemp = 85
+            elif float(sensor.average_weather_high_temperature) < 85.0:
+                hightemp = 85
+            """ Set Low Temp """
+            if float(sensor.average_weather_low_temperature) > 50.0:
+                lowtemp = 55
+            elif float(sensor.average_weather_low_temperature) < 50.0:
+                lowtemp = 55
+            """ Send the actual command to the thermostat """
+            climate.set_temperature(entity_id="climate.thermostat_2",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
+            pass
         pass
+
 
 @state_trigger("group.device_trackers == 'home'")
 def themostat_returned():
@@ -33,16 +37,16 @@ def themostat_returned():
     if trig_info["trigger_type"] == "timeout":
         pass
     else:
-        """Set High Temp"""
+        """ Set High Temp """
         if float(sensor.average_weather_high_temperature) > 85.0:
             hightemp = 75
         elif float(sensor.average_weather_high_temperature) < 85.0:
             hightemp = 85
-        """Set Low Temp"""
+        """ Set Low Temp """
         if float(sensor.average_weather_low_temperature) > 50.0:
             lowtemp = 55
         elif float(sensor.average_weather_low_temperature) < 50.0:
             lowtemp = 65
-        """Send the actual command to the thermostat"""
+        """ Send the actual command to the thermostat """
         climate.set_temperature(entity_id="climate.thermostat_2",target_temp_low=lowtemp,target_temp_high=hightemp,hvac_mode="heat_cool")
         pass
